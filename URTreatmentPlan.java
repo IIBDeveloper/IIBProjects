3114f3095baa99e57dccee5600660d4d0be8f6d1
wonde <developerdeveloperemail@gmail.com>
2019-09-24 12:48:58 -0700

    Java class are added

--- URTreatmentPlan.java
+++ URTreatmentPlan.java
@@ -6,9 +6,8 @@ should start with the upper case letter
 - it should be a noun such as color, button, system etc
 e.g TriggerControl
 */
-class TriggerControl{
+class UrTreatmentPlane{
 	public static void main(String args[]){
-		System.out.println("This is trigger message flow project");
-		//comment
+		System.out.println("This is Ur treatment plane message flow project");
 	}
 }
