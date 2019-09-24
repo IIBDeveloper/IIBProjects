f29f4a5acf797c7d90f9f5d41a666f10a7a2273c
wonde <developerdeveloperemail@gmail.com>
2019-09-24 07:49:41 -0700

    Added README.MD, TriggerControl.Java and TriggerControl.Class

---
+++ TriggerControl.java
@@ -0,0 +1,13 @@
+
+//CLASS NAMING CONVENTION
+/*
+CLASS 	
+should start with the upper case letter
+- it should be a noun such as color, button, system etc
+e.g TriggerControl
+*/
+class TriggerControl{
+	public static void main(String args[]){
+		System.out.println("This is trigger message flow project");
+	}
+}
